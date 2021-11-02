d8217564ca14f934a9f7f2e300cd8b81d272531b
William Gotterer <william.gotterer@gmail.com>
2021-11-02 12:08:12 -0400

    commit tree 42b0690ab18cfb46064f84f889db82336d582663

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog
+
+    # attr_reader :name
+
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed 
+    end
+end
+
+
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person 
+
+    def initialize(name, breed="Mutt")
+        @name = name
+    end
+end
+
